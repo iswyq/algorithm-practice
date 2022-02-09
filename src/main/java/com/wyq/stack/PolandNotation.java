35d4253a548b40c62a060cce0f018970cf596ed4
wyq <wangmounttai@gmail.com>
2022-02-09 12:01:10 +0800

    中缀转后缀+逆波兰表达式计算器实现

--- src/main/java/com/wyq/stack/PolandNotation.java
+++ src/main/java/com/wyq/stack/PolandNotation.java
@@ -11,12 +11,81 @@ public class PolandNotation {
     public static void main(String[] args) {
         //(30+4)*5-6
         String suffixExpression = "30 4 + 5 * 6 -";//后缀表达式
+        String expression = "1+((20+3)*5)-5";
         /*
          * 将后缀表达式放到List中去
          * */
+        List<String> list = toInfixExpressionList(expression);
         List<String> lst = getListString(suffixExpression);
-        int res = claculate(lst);
+        List<String> suffixList = parseSuffixExpressionList(list);
+        System.out.println(list);
+        System.out.println(suffixList);
+        int res = claculate(suffixList);
         System.out.println("计算结果为=" + res);
+    }
+
+    //转换为后缀表达式
+    public static List<String> parseSuffixExpressionList(List<String> list) {
+        //符号栈s1
+        Stack<String> s1 = new Stack<>();
+        //中间结果s2
+        ArrayList<String> s2 = new ArrayList<>();
+        for (String item : list) {
+            //如果遇到操作数 则直接入栈
+            if (item.matches("\\d+")) {
+                s2.add(item);
+            } else if (item.equals("(")) {
+                //如果是遇到( ,则直接入栈
+                s1.push(item);
+            } else if (item.equals(")")) {
+                //如果是遇到),则一直寻找，直到找到(   匹配一对括号
+                while (!s1.peek().equals("(")) {
+                    //没有遇到(,则一直while循环
+                    s2.add(s1.pop());
+                }
+                //对s1的(出栈
+                s1.pop();
+            } else {
+                //如果遇到运算符，比较运算符的优先级
+                //优先级小于栈顶元素优先级，将s1栈顶的运算符弹出并压入到s2中，再次转到4.1与s1中心的栈顶运算符相比较
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) > Operation.getValue(item)) {
+                    //比较s1栈顶元素与当前元素的优先级
+                    s2.add(s1.pop());
+                }
+                s1.push(item);
+            }
+        }
+        //将s1中的元素出栈到s2中
+        while (s1.size() != 0) {
+            s2.add(s1.pop());
+        }
+        return s2;
+    }
+
+    //获得中缀表达式的list集合
+    public static List<String> toInfixExpressionList(String stringExpression) {
+        ArrayList<String> list = new ArrayList<>();
+        char ch;
+        String str = "";
+        int index;
+        int i = 0;//i用于字符串遍历的索引
+        while (i < stringExpression.length()) {
+            ch = stringExpression.charAt(i);
+            //当前字符是一个数字
+            if (ch < 48 || ch > 57) {
+                list.add("" + ch);
+                i++;
+            } else {
+                //str置空
+                str = "";
+                while (i < stringExpression.length() && stringExpression.charAt(i) >= 48 && stringExpression.charAt(i) <= 57) {
+                    str += stringExpression.charAt(i);
+                    i++;
+                }
+                list.add(str);
+            }
+        }
+        return list;
     }
 
     //进行计算
@@ -68,4 +137,35 @@ public class PolandNotation {
 
     }
 
+}
+
+//比较运算符优先级
+class Operation {
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
+
+    public static int getValue(String str) {
+        int res = 0;
+        switch (str) {
+            case "+":
+                res = ADD;
+                break;
+            case "-":
+                res = SUB;
+                break;
+            case "*":
+                res = MUL;
+                break;
+            case "/":
+                res = DIV;
+                break;
+            default:
+                System.out.println("没有该运算符，无法操作");
+                break;
+        }
+        return res;
+
+    }
 }
